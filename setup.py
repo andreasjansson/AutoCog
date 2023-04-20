0a6680beb16a32fc3fe358417d56d0f2e01b703d
andreasjansson <andreas@replicate.ai>
2023-04-20 13:15:15 +0200

    Fix packaging

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="autocog",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/andreasjansson/AutoCog",
     packages=find_packages(),
     install_requires=[
